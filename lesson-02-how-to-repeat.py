02e0d0661e8e2b851ebc8aaf5f41eb4ba0b9ee83
André Ferreira <andre.ferreira@codetrends.com.br>
2016-10-20 11:33:48 -0200

    :snake: Lesson 02 - How to repeat

---
+++ lesson-02-how-to-repeat.py
@@ -0,0 +1,45 @@
+# Define a procedure, square, that takes one number
+# as its input, and returns the square of that
+# number (result of multiplying
+# the number by itself).
+# To help you out, the code for sum(a,b) is below.
+
+def sum(a,b):
+    c = a + b
+    return c
+
+def square(a):
+    return a * a
+
+# To test your procedure, uncomment the print
+# statement below, by removing the hash mark (#)
+# at the beginning of the line.
+
+# Do not remove the # from in front of the line
+# with the arrows (>>>). Lines which begin like
+# this (#>>>) are included to show the results
+# you should see when run your procedure.
+
+print square(5)
+#>>> 25
+
+# ----------------------------------------------------------------- #
+
+# Define a procedure, sum3, that takes three
+# inputs, and returns the sum of the three
+# input numbers.
+# To help you out, the code for sum(a,b) is below.
+
+def sum(a,b):
+    return a + b
+
+def sum3(a, b, c):
+    return a + b + c
+
+print sum3(1,2,3)
+#>>> 6
+
+print sum3(93,53,70)
+#>>> 216
+
+# ----------------------------------------------------------------- #
