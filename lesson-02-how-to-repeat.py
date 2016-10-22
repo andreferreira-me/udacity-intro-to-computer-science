f2b5f229391ce6f4055956b5e055ee9dde5405b2
André Ferreira <contato@andreferreira.me>
2016-10-22 18:42:50 +0000

    :snake: Lesson 2: How to Repeat

--- lesson-02-how-to-repeat.py
+++ lesson-02-how-to-repeat.py
@@ -43,3 +43,221 @@ print sum3(93,53,70)
 #>>> 216
 
 # ----------------------------------------------------------------- #
+
+# Define a procedure, abbaize, that takes
+# two strings as its inputs, and returns
+# a string that is the first input,
+# followed by two repetitions of the second input,
+# followed by the first input.
+
+def abbaize(a, b):
+    return a + b + b + a
+
+print abbaize('a','b')
+#>>> 'abba'
+
+print abbaize('dog','cat')
+#>>> 'dogcatcatdog'
+
+# ----------------------------------------------------------------- #
+
+# Define a procedure, find_second, that takes
+# two strings as its inputs: a search string
+# and a target string. It should return a
+# number that is the position of the second
+# occurrence of the target string in the
+# search string.
+
+def find_second(t, s):
+    first = t.find(s)
+    return t.find(s, first + 1)
+
+danton = "De l'audace, encore de l'audace, toujours de l'audace"
+print find_second(danton, 'audace')
+#>>> 25
+
+twister = "she sells seashells by the seashore"
+print find_second(twister,'she')
+#>>> 13
+
+# ----------------------------------------------------------------- #
+
+# Define a procedure, bigger, that takes in
+# two numbers as inputs, and returns the
+# greater of the two inputs.
+
+def bigger(n1, n2):
+    if n1 > n2:
+        return n1
+    else:
+        return n2
+
+print bigger(2,7)
+#>>> 7
+
+print bigger(3,2)
+#>>> 3
+
+print bigger(3,3)
+#>>> 3
+
+# ----------------------------------------------------------------- #
+
+# Define a procedure, is_friend, that
+# takes a string as its input, and
+# returns a Boolean indicating if
+# the input string is the name of
+# a friend. Assume I am friends with
+# everyone whose name starts with D
+# and no one else. You do not need to
+# check for the lower case 'd'
+
+def is_friend(s):
+    return s[0] == 'D'
+
+print is_friend('Diane')
+#>>> True
+
+print is_friend('Fred')
+#>>> False
+
+# ----------------------------------------------------------------- #
+
+# Define a procedure, is_friend, that takes
+# a string as its input, and returns a
+# Boolean indicating if the input string
+# is the name of a friend. Assume
+# I am friends with everyone whose name
+# starts with either 'D' or 'N', but no one
+# else. You do not need to check for
+# lower case 'd' or 'n'
+
+def is_friend(s):
+    if s[0] == 'D' or s[0] == 'N':
+        return True
+
+    return False
+
+print is_friend('Diane')
+#>>> True
+
+print is_friend('Ned')
+#>>> True
+
+print is_friend('Moe')
+#>>> False
+
+# ----------------------------------------------------------------- #
+
+# Define a procedure, biggest, that takes three
+# numbers as inputs and returns the largest of
+# those three numbers.
+
+def biggest(a, b, c):
+
+    if a > b:
+        if a > c:
+            return a
+        else:
+            return c
+    else:
+        if b > c:
+            return b
+        else:
+            return c
+
+    # Other solutions #
+    #return max(n1, n2, n3)
+
+    #return bigger(bigger(n1, n2), n3)
+
+print biggest(3, 6, 9)
+#>>> 9
+
+print biggest(6, 9, 3)
+#>>> 9
+
+print biggest(9, 3, 6)
+#>>> 9
+
+print biggest(3, 3, 9)
+#>>> 9
+
+print biggest(9, 3, 9)
+#>>> 9
+
+# ----------------------------------------------------------------- #
+
+# Define a procedure, print_numbers, that takes
+# as input a positive whole number, and prints
+# out all the whole numbers from 1 to the input
+# number.
+
+# Make sure your procedure prints "upwards", so
+# from 1 up to the input number.
+
+def print_numbers(n):
+    i = 0
+    while i < n:
+        i = i + 1
+        print i
+
+print_numbers(3)
+#>>> 1
+#>>> 2
+#>>> 3
+
+# ----------------------------------------------------------------- #
+
+# Define a procedure, factorial, that
+# takes one number as its input
+# and returns the factorial of
+# that number.
+
+def factorial(n):
+    r = 1
+    while n >= 1:
+        r = r * n
+        n = n - 1
+
+    return r
+
+print factorial(4)
+#>>> 24
+print factorial(5)
+#>>> 120
+print factorial(6)
+#>>> 720
+
+# ----------------------------------------------------------------- #
+
+# Modify the get_next_target procedure so that
+# if there is a link it behaves as before, but
+# if there is no link tag in the input string,
+# it returns None, 0.
+
+# Note that None is not a string and so should
+# not be enclosed in quotes.
+
+# Also note that your answer will appear in
+# parentheses if you print it.
+
+def get_next_target(page):
+    start_link = page.find('<a href=')
+
+    if (start_link == -1):
+        return None, 0
+
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
+
+# TestCases
+
+page = 'hi this is a "string" with no link'
+#>>> (None, 0)
+page = 'this is a <a href="http://udacity.com">link!</a>'
+#>>> ('http://udacity.com', 37)
+
+print get_next_target(page)
